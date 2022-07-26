355e5723d3450aa25d0c6e653fd8a32ced9c8998
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2022-07-26 12:35:18 -0400

    morging changes

--- browser/js/browser.js
+++ browser/js/browser.js
@@ -1,18 +1,34 @@
 const btnS = document.querySelector("#submit");
+const todos_div = document.querySelector(".todos");
+
+function outputToDos(data){
+
+    todos_div.innerHTML = "";
+    data.forEach((obj) => {
+        const html=`
+        <div class="todo">
+            <h3>${obj.text}</h3>
+            <button data-id="${obj.id}">Delete</button>
+        </div>
+        `;
+
+        todos_div.insertAdjacentHTML("beforeend",html);
+    });
+}
+
 function getTodos(){
     fetch("/api/todos")
     .then(res => res.json())
     .then(data => {
-        console.log(data);
+        outputToDos(data);
     });
-
 }
 
 function addTodo(event){
     const input = document.querySelector('input[name="something"]');
     const value =input.value;
     const data = {
-        noodles: value
+        text: value
     };
 
     event.preventDefault();
@@ -25,10 +41,37 @@ function addTodo(event){
         },
         body: JSON.stringify(data)
     }).then(res => res.json())
-    .then(res =>{
-        console.log(res)
+    .then(todos =>{
+        getTodos();
+        console.log(todos);
     })
+}
+function deleteDoto(event){
+    const el = event.target
+
+    console.log(el.tagName);
+
+    if(el.tagName === "BUTTON"){
+        const id = el.dataset.id;
+        const data = {
+            id: parseInt(id),
+            
+        }
+
+        fetch("/api/todos",{
+            method: "delete",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+        .then(todos => outputToDos(todos));
+    }
+
 }
 
 getTodos();
 btnS.addEventListener("click",addTodo);
+
+todos_div.addEventListener("click",deleteDoto);
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const { response, json } = require("express");
 const fs =require("fs");
+const { request } = require("http");
 const path = require("path");
+const db_path = path.join(__dirname,"../db/todos.json");
 
 
 function getTodoData(){
-    return fs.promises.readFile(path.join(__dirname,"../db/todos.json"),"utf8")
+    return fs.promises.readFile(db_path,"utf8")
     .then(data => JSON.parse(data));
 }
 
@@ -13,6 +15,9 @@ router.get("/todos", (req,res)=>{
     getTodoData()
     .then(todo_data =>{
         res.json(todo_data);
+        // todo_data.forEach(element => {
+        //     console.log(element);
+        // });
     })
     .catch(err => console.log(err));
 });
@@ -20,12 +25,38 @@ router.get("/todos", (req,res)=>{
 
 
 router.post("/todos",(req,res)=>{
-    console.log(req.body);
-    res.json({
-        messege: "Todo recived!"
-    })
+    getTodoData()
+    .then(todo_data =>{
+        const new_todo =req.body;
+        console.log("request body "+new_todo);
 
+        const reference_id = todo_data[todo_data.length - 1].id;
+        new_todo.id =reference_id+1;
+        console.log("index array "+reference_id);
+
+        todo_data.push(new_todo);
+
+        fs.promises.writeFile(db_path, JSON.stringify(todo_data,null,2))
+        .then(()=>{
+            console.log("todo Added Succesfully");
+            res.json(todo_data)
+        }).catch(err => console.log(err));
+        // console.log(req.body);
+        // fs.writeFile();
+        // res.json({
+        //     messege: "Todo recived!"
+        // })
+
+    });
 });
+
+//localhost DELETE request to the Database.
+
+router.delete("/todo",(req ,res)=>{
+    getTodoData()
+    .then
+});
+    
 
 
 
