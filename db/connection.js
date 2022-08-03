f9bdf10fa190c10e73fe2f4db71f03a197e50c6c
cr7st74n <cris-082011@hotmail.com>
2022-08-03 17:09:37 -0500

    adding connection to a remote server in heroku

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql2");
-
-// connecting to de database
-const connection = mysql.createPool({
+const is_on_heroku = process.env.NODE_ENV === "production";
+const connection_data = is_on_heroku ? {
+    host: "y6aj3qju8efqj0w1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    database: "r96a3vfua8byevgo",
+    user: "g2508w36cewbipbb",
+    password:'fojxb86akvwqhril'
+}: {
     host: "localhost",
     database: "class_expresss1",
     user: "root",
-    password:"12Mnjcgr."
-});
+    password:''
+}
+
+const connection =mysql.createPool(connection_data);
+
 
 module.exports = connection;
