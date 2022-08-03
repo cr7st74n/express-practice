1b8217acb9a5cebee074edf88fd569b26a3626f1
cr7st74n <cris-082011@hotmail.com>
2022-08-03 16:18:51 -0500

    ready for heroku

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,8 @@
 USE class_expresss1;
 
--- INSERT INTO todos1 (words) VALUES
---   ("first todo"),
---   ("second todo"),
---   ("third todo");
+INSERT INTO todos1 (words) VALUES
+  ("first todo"),
+  ("second todo"),
+  ("third todo");
 
-DELETE FROM todos1  WHERE id = 1;
+-- DELETE FROM todos1  WHERE id = 1;
