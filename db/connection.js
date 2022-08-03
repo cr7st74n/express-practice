9c4503083edad2ac2b85c9169a4d3c6ae2c9eb8c
cr7st74n <cris-082011@hotmail.com>
2022-08-03 17:18:13 -0500

    new web-server data

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 const is_on_heroku = process.env.NODE_ENV === "production";
 const connection_data = is_on_heroku ? {
     host: "y6aj3qju8efqj0w1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    database: "r96a3vfua8byevgo",
-    user: "g2508w36cewbipbb",
-    password:'fojxb86akvwqhril'
+    database: "fcd4c91bnakqgth9",
+    user: "xha5y4c1b3qhhphf",
+    password:'f8y3h7hzgtd2nwnm'
 }: {
     host: "localhost",
     database: "class_expresss1",
